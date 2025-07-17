60c3e9343090eb24b30c7b78dc28ea7cf9638359
Koustav Chatterjee <koustav2908@gmail.com>
2025-07-17 22:19:19 +0530

    Add: feature descriptions and sample data

---
+++ descriptions.py
@@ -0,0 +1,70 @@
+features = [
+    (
+        "alcohol",
+        "Alcohol Content",
+    ),
+    (
+        "malic_acid",
+        "Malic Acid Concentration",
+    ),
+    (
+        "ash",
+        "Ash Content (minerals)",
+    ),
+    (
+        "alcalinity_of_ash",
+        "Alkalinity of Ash",
+    ),
+    (
+        "magnesium",
+        "Magnesium Content (parts per million)",
+    ),
+    (
+        "total_phenols",
+        "Total Phenols Content",
+    ),
+    (
+        "flavanoids",
+        "Flavanoids",
+    ),
+    (
+        "nonflavanoid_phenols",
+        "Nonflavanoid Phenols",
+    ),
+    (
+        "proanthocyanins",
+        "Proanthocyanins",
+    ),
+    (
+        "color_intensity",
+        "Color Intensity",
+    ),
+    (
+        "hue",
+        "Hue",
+    ),
+    (
+        "od280/od315_of_diluted_wines",
+        "OD280/OD315 of Diluted Wines",
+    ),
+    (
+        "proline",
+        "Proline",
+    ),
+]
+
+sample_data = {
+    "alcohol": 13.97,
+    "malic_acid": 5.61,
+    "ash": 2.39,
+    "alcalinity_of_ash": 16.1,
+    "magnesium": 98.0,
+    "total_phenols": 2.90,
+    "flavanoids": 0.62,
+    "nonflavanoid_phenols": 0.55,
+    "proanthocyanins": 1.32,
+    "color_intensity": 5.72,
+    "hue": 0.64,
+    "od280/od315_of_diluted_wines": 2.52,
+    "proline": 751.0,
+}
